e4f63945c8297bd34667b8066cd67c87d8d72d71
marianomontone <marianomontone@gmail.com>
2009-11-28 16:36:32 -0300

    Some (failing) cl-url tests
    Ignore-this: b98a2030809a3141ebc1d6c3dc6740ac
    
    darcs-hash:20091128193632-5b2f2-3930f629fcf6e489822f0a64c17bca66ce3d3259.gz

--- deps/cl-url/test.lisp
+++ deps/cl-url/test.lisp
@@ -4,14 +4,13 @@
 
 (in-suite cl-url-tests)
 
-(defun run-test ()
+(defun run-tests ()
   (run 'cl-url-tests))
 
 ;reader-syntax:   #url"http://localhost/myapp/main.do"
 
-#|
 (test url-reader-syntax-test
-  (is (equal-url #url"/" (make-url)))
+  (is (equal-url #u"/" (make-url))))
   (is (equal-url #url"" (make-url)))
   (is (equal-url #url"//localhost") (make-url :hostname "localhost"))
   (is (equal-url #url"foo/bar") (make-url :path (list "foo")
@@ -23,11 +22,12 @@
   (is (equal-url #url"action.do") (make-url :program "action.do"))
   (is (equal-url #url"http:") (make-url :protocol :http))
   (is (equal-url #url"?a1=v1&a2=v2" (make-url :parameters '(:a1 v1 :a2 v2))))
-  (is (equal-url #url":80") (make-url :port 80))
+  (is (equal-url #url"//:80") (make-url :port 80))
   (is (equal-url #url"//localhost:80") (make-url :hostname "localhost"
 						 :port 80)))
 
-|#
+
+
 
 
 
