fb5933d6bf028009ac1d0d5833769a22145e2c37
mmontone <marianomontone@gmail.com>
2014-02-12 20:11:12 -0300

    Trying component embedding

--- src/component/showcase/counters.lisp
+++ src/component/showcase/counters.lisp
@@ -11,3 +11,24 @@
 (defmethod render ((counters counters-showcase))
   (loop for counter in (child-components counters)
        do (render counter)))
+
+(defcomponent empty-component () ())
+
+(defcomponent embedded-counter ()
+  ((body :component t
+	 :initform (make-instance 'empty-component)
+	 :accessor body)
+   (counter :component t
+	    :initform (make-instance 'counter :value 0)
+	    :accessor counter)))
+
+(define-renderer ((self embedded-counter))
+  (render (counter self))
+  (if (typep (body self) 'empty-component)
+      (htm (:a :href (action-link 'embed-counter)
+	       "Embed counter"))
+      ;; else
+      (render (body self))))
+
+(define-action embed-counter (self &rest args)
+  (setf (body self) (make-instance 'embedded-counter)))
